f6b8f63647c2f7be4ef73efe5f9eede2409dc6a0
Smoxsan <smox@DESKTOP-C9JQ6RG>
2024-11-05 15:29:52 +0100

    error ft_printf

--- ft_print.c
+++ ft_print.c
@@ -6,55 +6,55 @@
 /*   By: smox <smox@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:30:06 by fkonig            #+#    #+#             */
-/*   Updated: 2024/11/05 15:19:05 by smox             ###   ########.fr       */
+/*   Updated: 2024/11/05 15:28:56 by smox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-satic void	handel_specifier(char speci, va_list args)
+void	handel_specifier(char speci, va_list args)
 {
 	char			c;
 	char			*s;
 	void			*p;
 	int				d;
 	unsigned int	u;
-	unsigned int 	x:
+	unsigned int 	x;
 	unsigned int 	X;
 
-	if (*a == 'c')
+	if (speci == 'c')
 	{
 		c = (char)va_arg(args, int);
 		ft_printf_c(c);
 	}
-	else if (*a == 's')
+	else if (speci == 's')
 	{
-		*s = va_arg(args, char*);
+		s = va_arg(args, char*);
 		ft_printf_s(s);
 	}
-	else if (*a == 'p')
+	else if (speci == 'p')
 	{
-		*p = va_arg(args, void*);
+		p = va_arg(args, void*);
 		ft_printf_p(p);
 	}
-	else if (*a == 'd' || *a == 'i')
+	else if (speci == 'd' || speci == 'i')
 	{
 		d = va_arg(args, int);
 		ft_printf_d(d);
 	}
-	else if (*a == '%')
+	else if (speci == '%')
 		ft_printf_per();
-	else if (*a == 'u')
+	else if (speci == 'u')
 	{
 		u = va_arg(args, unsigned int);
 		ft_printf_u(u);
 	}
-	else if (*a == 'x')
+	else if (speci == 'x')
 	{
 		x = va_arg(args, unsigned int);
 		ft_printf_hexlower(x);
 	}
-	else if (*a == 'X')
+	else if (speci == 'X')
 	{
 		X = va_arg(args, unsigned int);
 		ft_printf_hexupper(X);
